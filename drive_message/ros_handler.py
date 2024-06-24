lidar heading & add header VehicleState (using /fix header)& set tf broadcaster with VehicleState timestamp & add base_link tf

drive_message/ros_handler.py
@@ -48,11 +48,11 @@ class ROSHandler():
     def set_subscriber_protocol(self):
         rospy.Subscriber('/CANOutput', CANOutput, self.can_output_cb)
         rospy.Subscriber('/NavigationData', NavigationData, self.navigation_data_cb)
-        rospy.Subscriber('/nmea_sentence', Sentence, self.nmea_sentence_cb)
+        #rospy.Subscriber('/nmea_sentence', Sentence, self.nmea_sentence_cb)
         rospy.Subscriber('/UserInput', UserInput, self.user_input_cb)
         rospy.Subscriber('/control/target_actuator', Actuator, self.target_actuator_cb)
-        #rospy.Subscriber('/fix', NavSatFix, self.nav_sat_fix_cb)
-        #rospy.Subscriber('/heading', QuaternionStamped, self.heading_cb)
+        rospy.Subscriber('/fix', NavSatFix, self.nav_sat_fix_cb)
+        rospy.Subscriber('/heading', QuaternionStamped, self.heading_cb)
         rospy.Subscriber('/simulator/objects', PoseArray, self.sim_objects_cb)
 
         if not USE_LIDAR:
@@ -99,6 +99,7 @@ class ROSHandler():
                 self.vehicle_state.heading.data = parsed[0]
 
     def nav_sat_fix_cb(self, msg):  # nmea_sentence error handling
+        self.vehicle_state.header = msg.header
         if not check_error(self.vehicle_state.position.x, msg.latitude, 30):
             self.vehicle_state.position.x = msg.latitude
         if not check_error(self.vehicle_state.position.y, msg.longitude, 30):
@@ -152,10 +153,17 @@ class ROSHandler():
                 return
             else:
                 x,y = conv
+                q = obj.pose.orientation
+                siny_cosp = 2 * (q.w * q.z + q.x * q.y)
+                cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z)
+                z_angle_rad = np.arctan2(siny_cosp, cosy_cosp)
+                z_angle_deg = np.degrees(z_angle_rad)
+                
             object_info.position.x = x
             object_info.position.y = y
             object_info.velocity.data = obj.value
-            object_info.heading.data = obj.pose.orientation.z
+            object_info.heading.data = z_angle_deg
+
             self.detection_data.objects.append(object_info)
             
     def system_to_can(self, mode):

visualizer/visualizer.py
@@ -18,16 +18,18 @@ class Visualizer:
         # calibration
         self.static_br = tf2_ros.StaticTransformBroadcaster()
         static_transforms = [
+            ((1.3025, 0.0, 1.5), (0, 0, 0, 1), 'base_link', 'ego_car'),  # center
             ((1.1, 0.0, 1.5), (0, 0, 0, 1), 'os_sensor', 'ego_car'),  # lidar
             ((1.8, 0.0, 1.0), (0, 0, 0, 1), 'front', 'ego_car'),     # front camera
             ((1.7, 0.7, 1.0), (0, 0, 0, 1), 'left_front', 'ego_car'),# left 
-            ((1.6, 0.7, 1.0), (0, 0, 0, 1), 'left_rear', 'ego_car'), # left
+            ((1.4, 0.7, 1.0), (0, 0, 0, 1), 'left_rear', 'ego_car'), # left
             ((1.7, -0.7, 1.0), (0, 0, 0, 1), 'right_front', 'ego_car'), # right
-            ((1.6, -0.7, 1.0), (0, 0, 0, 1), 'right_rear', 'ego_car')  # right
+            ((1.4, -0.7, 1.0), (0, 0, 0, 1), 'right_rear', 'ego_car')  # right
         ]
         self.publish_static_tfs(static_transforms)
 
         self.ego_pos = [0.0, 0.0]
+        self.ego_time = rospy.Time(0)
 
         self.pub_viz_car = rospy.Publisher('/visualizer/car', Marker, queue_size=1)
         self.pub_viz_car_info = rospy.Publisher('/visualizer/car_info', Marker, queue_size=1)
@@ -47,10 +49,14 @@ class Visualizer:
         info = f"{(v*3.6):.2f}km/h {yaw:.2f}deg"
         self.ego_car_info.text = info
         quaternion = tf.transformations.quaternion_from_euler(math.radians(0), math.radians(0), math.radians(yaw))  # RPY
-        self.br.sendTransform(
-            (self.ego_pos[0], self.ego_pos[1], 0),
-            (quaternion[0], quaternion[1],quaternion[2], quaternion[3]),
-            rospy.Time.now(),'ego_car','world')    
+        
+        if self.ego_time != msg.header.stamp:
+            self.br.sendTransform(
+                (self.ego_pos[0], self.ego_pos[1], 0),
+                (quaternion[0], quaternion[1],quaternion[2], quaternion[3]),
+                self.ego_time,'ego_car','world')
+            self.ego_time = msg.header.stamp
+        
         self.pub_viz_car.publish(self.ego_car)
         self.pub_viz_car_info.publish(self.ego_car_info)
     
@@ -74,7 +80,7 @@ class Visualizer:
         static_transformStamped_vec = []
         for translation, rotation, child_frame, parent_frame in transforms:
             static_transformStamped = geometry_msgs.msg.TransformStamped()
-            static_transformStamped.header.stamp = rospy.Time.now()
+            #static_transformStamped.header.stamp = rospy.Time(0)
             static_transformStamped.header.frame_id = parent_frame
             static_transformStamped.child_frame_id = child_frame
             static_transformStamped.transform.translation.x = translation[0]