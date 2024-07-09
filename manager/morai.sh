c57df3dc3a9ca7bf39478b8ba8bcef26df3d86c1
kka-na <kimgana96@gmail.com>
2024-07-09 15:42:29 +0900

    [manger] Add morai bash:

---
+++ manager/morai.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ../ui/
+python3 ui.py & 
+cd ../visualizer
+python3 visualizer.py &
+cd ../map_lane
+python3 map_lane.py &
+cd ../simulator/
+python3 morai.py & 
+cd ../drive_message/
+python3 drive_message.py KIAPI_Racing & 
+cd ../control/
+python3 control.py &
+cd ../planning/
+python3 planning.py 
