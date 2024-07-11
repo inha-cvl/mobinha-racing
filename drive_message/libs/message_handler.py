b9e191ecccf5d79d8b656464d12622dd503f2982
kka-na <kimgana96@gmail.com>
2024-07-11 21:55:05 +0900

    merge

--- drive_message/libs/message_handler.py
+++ drive_message/libs/message_handler.py
@@ -121,12 +121,8 @@ def convert_local_to_enu(ego_pose, ego_heading, obj_pose):
         y = ego_pose[1]+ny
         
         return x,y
-
-def distance(point1, point2):
-    return np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-
-#TODO # lap_number
-def check_lap_count(cnt, local_pos, reference_point, radius=20, lap_flag=False):
+    
+def check_lap_count(cnt, local_pos, reference_point, lap_flag, radius=20):
     if local_pos is None:
         return cnt, lap_flag
     distance = math.sqrt((local_pos[0] - reference_point[0])**2 + (local_pos[1] - reference_point[1])**2)
