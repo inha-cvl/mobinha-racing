e6aea32fcf589d1b62155edef35a33978a04cf9a
kka-na <kimgana96@gmail.com>
2024-07-15 00:38:21 +0900

    [map_lane] Modify map caller

--- map_lane/map_viz.py
+++ map_lane/map_viz.py
@@ -1,4 +1,5 @@
 import rospy
+import time
 from hd_map.libs.map_utils import *
 from hd_map.map import MAP
 
@@ -11,8 +12,7 @@ mlmap_viz = map.mlmap_viz
 pub_lmap_viz = rospy.Publisher('/lmap', MarkerArray, queue_size=1)
 pub_mlmap_viz = rospy.Publisher('/mlmap', MarkerArray, queue_size=1)
 
-
-rate = rospy.Rate(0.01)
+rate = rospy.Rate(0.5)
 while not rospy.is_shutdown():
     pub_lmap_viz.publish(lmap_viz)
     pub_mlmap_viz.publish(mlmap_viz)
