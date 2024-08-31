ed9aacf05f125aa63b254fe5bfe26ab0a3456e04
inha-cvl <cvlab.inhauniv@gmail.com>
2024-08-31 23:52:35 +0900

    [transmiter]Add Front RDR FD

--- transmitter/can2_test.py
+++ transmitter/can2_test.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 # sudo ip link set can0 up type can bitrate 500000
+# sudo ip link set can0 type can bitrate 500000 sample-point 0.750 dbitrate 2000000 dsample-point 0.750 fd on
 import asyncio
 import can
+import cantools.database
 import rospy
 
 import sys
+import os
 import signal
+import cantools
 
 from transmitter_handler import TransmitterHandler
 
@@ -14,15 +18,20 @@ def signal_handler(sig, frame):
 
 class CAN2Test():
     def __init__(self):
-        self.bus = can.ThreadSafeBus(interface='socketcan', channel='can1', bitrate=500000)
+        self.bus = can.ThreadSafeBus(interface='socketcan',fd=True, channel='can0', bitrate=500000)
         self.TH = TransmitterHandler()
+        self.dbc = cantools.database.load_file('/home/inha/catkin_ws/src/mobinha-racing/transmitter/RDR.dbc')
 
     async def read_from_can(self):
         try:
             while not rospy.is_shutdown():
                 message = await asyncio.get_event_loop().run_in_executor(None, self.bus.recv, 0.2)               
                 if message:
-                    print(message)
+                    try:
+                        print(self.dbc.decode_message(message.arbitration_id, message.data)['Long01'])
+                        print('\n')
+                    except Exception as E:
+                        pass
         except Exception as e:
             rospy.logerr(f"Error in read_from_can: {e}")
         finally:
